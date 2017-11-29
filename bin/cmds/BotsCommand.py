6c77900b66bba28df175594442320b9c72d55e7e
NovaFox161 <novamaday@gmail.com>
2017-11-29 06:22:18 -0600

    It would seem I never committed this lol

--- bin/cmds/BotsCommand.py
+++ bin/cmds/BotsCommand.py
@@ -8,7 +8,7 @@ def run_command(message, client):
 	em.set_author(name="Flownyx")
 	em.description = "Bots I suggest you check out!"
 	em.add_field(name="DisCal", value="Advanced GoogleCalendar bot: https://www.cloudcraftgaming.com/discal",
-	             inline=False)
+				 inline=False)
 	em.add_field(name="Apparatus", value="Advanced multi-purpose bot: http://www.xaanit.me/Products/", inline=False)
 
 	asyncio.ensure_future(client.send_message(message.channel, embed=em))
--- bin/cmds/HelpCommand.py
+++ bin/cmds/HelpCommand.py
@@ -4,10 +4,10 @@ import discord
 
 
 def run_command(message, client):
-	em = discord.Embed(color=discord.Color.magenta())
-	em.set_author(name="Flownyx Help")
-	em.add_field(name="flow", value="Shows bot info", inline=False)
-	em.add_field(name="bots", value="List of bots I highly recommend", inline=False)
-	em.add_field(name="help", value="Displays commands obviously", inline=False)
+    em = discord.Embed(color=discord.Color.magenta())
+    em.set_author(name="Flownyx Help")
+    em.add_field(name="flow", value="Shows bot info", inline=False)
+    em.add_field(name="bots", value="List of bots I highly recommend", inline=False)
+    em.add_field(name="help", value="Displays commands obviously", inline=False)
 
-	asyncio.ensure_future(client.send_message(message.channel, embed=em))
+    asyncio.ensure_future(client.send_message(message.channel, embed=em))
